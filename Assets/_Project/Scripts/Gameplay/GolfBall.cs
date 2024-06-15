0f59c127908e53048bec44bf3da7a1c922b88f19
Bohdan Tsaplia <bohdan.tsaplia@udtech.global>
2024-06-15 17:54:34 +0300

    move golf ball controls to a new input system

--- Assets/_Project/Scripts/Gameplay/GolfBall.cs
+++ Assets/_Project/Scripts/Gameplay/GolfBall.cs
@@ -1,4 +1,7 @@
+using System;
+using gisha.golf;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace gishadev.golf.Gameplay
 {
@@ -16,30 +19,51 @@ namespace gishadev.golf.Gameplay
         private Rigidbody2D _rb;
         private Camera _cam;
         private LineRenderer _lr;
-
+        private CustomInput _input;
+        private bool _isClubDown;
+        
         private void Awake()
         {
             _cam = Camera.main;
             _rb = GetComponent<Rigidbody2D>();
             _lr = GetComponentInChildren<LineRenderer>();
+            _input = new CustomInput();
+        }
+
+        private void OnEnable()
+        {
+            _input.Enable();
+            _input.Gameplay.ClubDown.performed += OnClubDown;
+            _input.Gameplay.ClubUp.performed += OnClubUp;
+        }
+
+        private void OnDisable()
+        {
+            _input.Disable();
+            _input.Gameplay.ClubDown.performed -= OnClubDown;
+            _input.Gameplay.ClubUp.performed -= OnClubUp;
+        }
+        
+        private void OnClubUp(InputAction.CallbackContext obj)
+        {
+            _lr.enabled = false;
+            _isClubDown = false;
+            _rb.AddForce(PunchDirection * (PunchForcePercentage * maxPunchForce), ForceMode2D.Impulse);
+        }
+
+        private void OnClubDown(InputAction.CallbackContext obj)
+        {
+            _lr.enabled = true;
+            _isClubDown = true;
         }
 
         private void Update()
         {
-            if (Input.GetMouseButtonDown(0))
-                _lr.enabled = true;
-
-            if (Input.GetMouseButtonUp(0))
-            {
-                _lr.enabled = false;
-                _rb.AddForce(PunchDirection * (PunchForcePercentage * maxPunchForce), ForceMode2D.Impulse);
-            }
-
-            if (Input.GetMouseButton(0))
+            if (_isClubDown)
             {
                 _mousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
 
-                var punchVector =  (Vector2) transform.position - _mousePos;
+                var punchVector = (Vector2) transform.position - _mousePos;
                 PunchDirection = punchVector.normalized;
                 PunchForcePercentage = Mathf.Min(punchVector.magnitude, maxLineLength) / maxLineLength;
 
