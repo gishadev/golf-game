d44462e010801ab006de668ae8f2a2539291f7cd
Bohdan Tsaplia <bohdan.tsaplia@udtech.global>
2024-06-15 18:15:50 +0300

    add ball in a hole triggering

---
+++ Assets/_Project/Scripts/Core/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using gishadev.golf.Gameplay;
+using UnityEngine;
+
+namespace gishadev.golf.Core
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static Action Won;
+        
+        private void Awake()
+        {
+            Hole.BallEnteredHole += OnBallEnteredHole;
+        }
+
+        private void OnDestroy()
+        {
+            Hole.BallEnteredHole -= OnBallEnteredHole;
+        }
+        
+        private void OnBallEnteredHole()
+        {
+            Debug.Log("Ball entered hole");
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Gameplay/Hole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace gishadev.golf.Gameplay
+{
+    public class Hole : MonoBehaviour
+    {
+        public static Action BallEnteredHole;
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.TryGetComponent(out GolfBall golfBall))
+                BallEnteredHole?.Invoke();
+        }
+    }
+}
